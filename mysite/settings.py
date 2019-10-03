d9ff8325b8318d64283c1ab14a1839fab7250266
yewone <yewonez@naver.com>
2019-10-03 11:16:00 +0000

    Change 3rd week final

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@x9*(fz@r*kbrq36vxdvhs1pi(ko!&#4p$wky04i%vwd#(gs%m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
